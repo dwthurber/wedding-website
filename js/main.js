77c010785cc0dd25ef68b1232dd0bee043388ca3
Derek Thurber <derek.thurber@northwestern.edu>
2017-04-19 15:55:32 -0500

    added rsvp side slider and gallery images

--- js/main.js
+++ js/main.js
@@ -124,4 +124,17 @@ jQuery(document).ready(function($){
 		//create a svgClippedSlider object for each .cd-svg-clipped-slider
 		new svgClippedSlider($(this));
 	});
+  
+    //open the lateral panel
+	$('.cd-btn').on('click', function(event){
+		event.preventDefault();
+		$('.cd-panel').addClass('is-visible');
+	});
+	//clode the lateral panel
+	$('.cd-panel').on('click', function(event){
+		if( $(event.target).is('.cd-panel') || $(event.target).is('.cd-panel-close') ) { 
+			$('.cd-panel').removeClass('is-visible');
+			event.preventDefault();
+		}
+	});
 });
